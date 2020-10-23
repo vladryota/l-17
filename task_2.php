c523aa87d9cc2fdf222da9ab800a8e99ec0fff77
Lakky <vladryota@gmail.com>
2020-10-23 19:27:16 +0300

    ytfd

--- task_2.php
+++ task_2.php
@@ -46,7 +46,7 @@
                                       "text" => "<p>We collect certain data through cookies and similar technologies (e.g. web beacons, tags, device identifiers). Cookies are text files placed on your computer to collect standard internet log information and visitor behaviour information. This information is used to track visitor use of the website and to compile statistical reports on website activity. We register your interaction with our services in order to improve our website, content and services. Our use of such technologies and the data collected is described in more detail in our Cookie Policy. You can manage your cookie preferences through your browser settings.</p>"
 
                                   ]
-                            ] ?>
+                            ]; ?>
                             <div class="fs-lg fw-300 p-5 bg-white border-faded rounded mb-g">
                                 <?php   foreach ($namePost as $namePosts): // пеербираем массив
                                 echo $namePosts['post']; // выводим
